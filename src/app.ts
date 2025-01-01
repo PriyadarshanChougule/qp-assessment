d006661903b7479195135be569e5088a9cf89d03
priyadarshanchougule <priyadarshanchougule@gmail.com>
2025-01-01 20:51:09 +0530

    removing sample code

--- src/app.ts
+++ src/app.ts
@@ -35,45 +35,7 @@ const connectWithRetry = () => {
         // Sync models with { alter: true } to prevent table deletion in production
         sequelize.sync({ alter: true })
           .then(() => {
-            Product.count().then(count => {
-                if (count === 0) {
-                    // Only create product if the table is empty
-                    Product.create({
-                        productName: 'Sample Product',
-                        productDescription: 'This is a sample product description.',
-                        price: 29.99,
-                        category_id: 1,  // Assuming you have a category with ID 1
-                        brand: 'SampleBrand',
-                        weight: 1.5,  // 1.5 kg
-                        stock_quantity: 100,
-                        image_url: 'https://example.com/image.jpg',
-                        expiration_date: new Date('2025-12-31'),  // Example expiration date
-                        is_active: true,
-                        discount_price: 19.99,
-                        special_offer: '10% off on first purchase',
-                        unit_of_measure: 'kg',
-                    })
-                    .then(() => console.log('Sample product created'))
-                    .catch(err => console.error('Error creating sample product:', err));
-
-                    Order.create({
-                        userId: 1, // Example user ID
-                        orderDate: new Date(),
-                        status: 'Pending',
-                        amount: 100.0,
-                        paymentStatus: 'Not Paid',
-                        paymentMode: 'Credit Card',
-                        address: '123 Example St, Example City',
-                        deliveryDate: new Date('2025-01-01'),
-                        discount: 10.0,
-                        couponCode: 'NEWYEAR10',
-                        taxAmount: 5.0,
-                      }).then(() => console.log('Sample order created'))
-                      .catch(err => console.error('Error creating sample order:', err));
-  
-                    
-                }
-            });
+            
           })
           .catch((error) => console.error("Error syncing database:", error));
 
